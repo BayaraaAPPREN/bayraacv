d9c430a96aeead6bc8cf296c98304e689f8ac6c6
Doljko <bayarsuren0310@gmail.com>
2022-03-28 14:54:03 -0700

    dush

---
+++ pages/About.js
@@ -0,0 +1,9 @@
+export default function About(){
+    return(
+        <div className="grid grid-col">
+            <div className="justify-center items-center" >
+                Заа тэгж байгаад хийнээ багшаа
+            </div>
+        </div>
+    )
+}
